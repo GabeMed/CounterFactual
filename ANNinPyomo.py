277789af323de4345a8293574318c04d084abdac
GeraltSHEN <100735135+GeraltSHEN@users.noreply.github.com>
2023-03-03 15:15:00 -0500

    Update ANNinPyomo.py
    imp update: now allows you to find trajectory (though you need to manually determine the CF label)

--- ANNinPyomo.py
+++ ANNinPyomo.py
@@ -72,28 +72,32 @@ for layer_id, layer in enumerate(network_definition.layers):
     print(f"{layer_id}\t{layer}\t{layer.activation}")
 formulation = FullSpaceNNFormulation(network_definition)
 
-m = pe.ConcreteModel()
-# create an OMLT block for the neural network and build its formulation
-m.nn = OmltBlock()
-m.nn.build_formulation(formulation)
+# start our CF problem
 solver = pe.SolverFactory('gurobi')
-# Start Our CF formulation
-# set all the x as binary variables since we only have 0-1 in all the dimensions in this case
-for i in m.nn.inputs_set:
-    m.nn.inputs[i].domain = pe.Binary
-# set the output class index, 0 ~ 10
-m.IDX10 = pe.RangeSet(0, 10)
-# set the query cell index, 0 ~ 978
-m.IDX978 = pe.RangeSet(0, 978)
-# create a blank (init all zeros) factual parameter
-m.factual = pe.Param(m.IDX978, initialize={i: 0 for i in range(979)}, mutable=True)
-# objective fn is to minimize the distance between factual and counterfactual
-# in this case (all binary variables), we use manhattan distance
-# we simply square them since it is equivalent to calculating the absolute value
-m.obj = pe.Objective(expr= sum([(m.nn.inputs[i] - m.factual[i])**2 for i in m.IDX978]),
-                     sense= pe.minimize)
+def build_basic_model(formulation):
+    m = pe.ConcreteModel()
+    # create an OMLT block for the neural network and build its formulation
+    m.nn = OmltBlock()
+    m.nn.build_formulation(formulation)
+    # Start Our CF formulation
+    # set all the x as binary variables since we only have 0-1 in all the dimensions in this case
+    for i in m.nn.inputs_set:
+        m.nn.inputs[i].domain = pe.Binary
+    # set the output class index, 0 ~ 10
+    m.IDX10 = pe.RangeSet(0, 10)
+    # set the query cell index, 0 ~ 978
+    m.IDX978 = pe.RangeSet(0, 978)
+    # create a blank (init all zeros) factual parameter
+    m.factual = pe.Param(m.IDX978, initialize={i: 0 for i in range(979)}, mutable=True)
+    # objective fn is to minimize the distance between factual and counterfactual
+    # in this case (all binary variables), we use manhattan distance
+    # we simply square them since it is equivalent to calculating the absolute value
+    m.obj = pe.Objective(expr=sum([(m.nn.inputs[i] - m.factual[i]) ** 2 for i in m.IDX978]),
+                         sense=pe.minimize)
+    print('basic model built')
+    return m
 
-def generate_factual_param(query_data):
+def generate_factual_param(m, query_data):
     # create factual of interest as a parameter
     def generate_factual_dic(query_data):
         """
@@ -105,27 +109,36 @@ def generate_factual_param(query_data):
         """
         query_dataArr = query_data.numpy()
         x = query_dataArr[:-1]
-        label = query_dataArr[-1]
-        print(f'the factual cell type is type {label}, \n'
-              f'choose your counterfactual cell type in 0 ~ 10 except {label}.')
+        Flabel = query_dataArr[-1]
+        print(f'the factual cell type is type {Flabel}, \n'
+              f'choose your counterfactual cell type in 0 ~ 10 except {Flabel}.')
         factual_dic = {}
         for i in range(x.size):
             factual_dic[i] = x[i]
-        return factual_dic, label
-    factual_dic, label = generate_factual_dic(query_data)
+        return factual_dic, Flabel
+    factual_dic, Flabel = generate_factual_dic(query_data)
     # set values for facutal parameter
     for idx, value in factual_dic.items():
         m.factual[idx].value = value
     xF = query_data[:-1].numpy() # factual parameter in array form
-    return xF, label
+    return xF, int(Flabel)
 
-def search_nearest_CF(CFlabel, alpha):
-    # add constraint such that the prediction of the counterfactual is consistent with CFlabel assigned
-    # reformulated into inequality constraints that contains a confidence factor, alpha
-    def CFconsistency_rule(m, i):
-        return m.nn.outputs[CFlabel] >= m.nn.outputs[i] + pe.log(alpha)
-    m.CFconsistency = pe.Constraint(m.IDX10 - [CFlabel], rule=CFconsistency_rule)
-    solver.solve(m, tee=True)
+def search_nearest_CF(m, CFlabel, Flabel, alpha):
+    # add constraint that describes the ratio of probabilities of counterfactual and factual
+    # the ratio is controlled by a confidence factor, alpha
+    # >1 when 'forcing' counterfactual to be CFlabel cell type
+    # <1 when 'allowing' counterfactual to be the same as Flabel cell type
+    def CFandF_compete_rule(m):
+        return m.nn.outputs[CFlabel] >= m.nn.outputs[Flabel] + pe.log(alpha)
+    m.CFandF_compete = pe.Constraint(rule=CFandF_compete_rule)
+    # add constraints such that the probabilities of counterfactual and factual must be greater than other cell types
+    def CF_dominant_rule(m, i):
+        return m.nn.outputs[CFlabel] >= m.nn.outputs[i]
+    m.CF_dominant = pe.Constraint(m.IDX10 - [CFlabel, Flabel], rule=CF_dominant_rule)
+    def F_dominant_rule(m, i):
+        return m.nn.outputs[Flabel] >= m.nn.outputs[i]
+    m.F_dominant = pe.Constraint(m.IDX10 - [CFlabel, Flabel], rule=F_dominant_rule)
+    solver.solve(m, tee=False)
     # transform the xCF into numpy array
     xCF = np.zeros(979)
     for i in range(979):
@@ -140,13 +153,22 @@ def search_nearest_CF(CFlabel, alpha):
     yCF = np.exp(log_probabilities_tensor.detach().numpy())
     return xCF, yCF
 
-def solve_CF_one_CFlabel(query_data, CFlabel, alpha):
-    xF, label_F = generate_factual_param(query_data)
-    xCF, yCF = search_nearest_CF(CFlabel, alpha) # yCF is probabilities of each label
-    probability_CF = max(yCF)
+def solve_CF(formulation, query_data, CFlabel, alpha):
+    m = build_basic_model(formulation)
+    xF, Flabel = generate_factual_param(m, query_data)
+    xCF, yCF = search_nearest_CF(m, CFlabel, Flabel, alpha) # yCF is probabilities of each label
+    probability_CF = yCF[CFlabel]
+    probability_F = yCF[Flabel]
     label_CF = np.argmax(yCF)
-    print(f'the most possible cell type is {label_CF}, as the same as input CFlabel: {CFlabel}')
-    print(f'the most possible cell type is {label_CF}, with probability of : {round(probability_CF*100, 2)}%')
+    if label_CF == CFlabel:
+        print(f'the most possible cell type is {label_CF}, as the same as input CFlabel: {CFlabel}\n'
+              f'the most possible cell type is {label_CF}, with probability of : {round(probability_CF*100, 2)}% \n'
+              f'the second most possible cell type is {Flabel}, with probability of: {round(probability_F*100, 2)}%')
+    else:
+        print(f'the most possible cell type is {label_CF}, not the same as input CFlabel: {CFlabel}, \n'
+              f'but the same as input Flabel: {Flabel}\n'
+              f'the most possible cell type is {label_CF}, with probability of : {round(probability_F*100, 2)}% \n'
+              f'the second most possible cell type is {CFlabel}, with probability of : {round(probability_CF*100, 2)}%')
     distance_obj = m.obj()
     dx = xCF != xF
     print(f'nearest distance is {sum(bool(d) for d in dx)}, as the same as obj value {distance_obj}')
@@ -159,48 +181,35 @@ def solve_CF_one_CFlabel(query_data, CFlabel, alpha):
           f'dxF   : {dxF} \n'
           f'dCF   : {dxCF}')
     # remember to delete the constraint CFconsistency for the next run
-    m.del_component(m.CFconsistency)
-    return xCF, yCF, probability_CF, distance_obj, dx, dx_position, dxpair, dxF, dxCF
+    del m
+    return xCF, yCF, probability_CF, probability_F, distance_obj, dx, dx_position, dxpair, dxF, dxCF
 
-def solve_CF_all_CFlabels(query_data, alpha):
-    XCF = {}
-    YCF = {}
-    probabilities_CF = {}
-    distances = {}
-    dxs = {}
-    dx_positions ={}
-    dxpairs = {}
-    dxFs = {}
-    dxCFs = {}
-    for CFlabel in [i for i in range(11) if i != query_data[-1]]:
-        xCF, yCF, probability_CF, distance_obj, dx, dx_position, dxpair, dxF, dxCF = solve_CF_one_CFlabel(query_data, CFlabel, alpha)
-        XCF[CFlabel] = xCF
-        YCF[CFlabel] = yCF
-        probabilities_CF[CFlabel] = probability_CF
-        distances[CFlabel] = distance_obj
-        dx_positions[CFlabel] = dx_position
-        dxpairs[CFlabel] = dxpair
-        dxFs[CFlabel] = dxF
-        dxCFs[CFlabel] = dxCF
-    min_distance_CFlabel = min(distances, key=distances.get)
-    nearest_xCF = XCF[min_distance_CFlabel]
-    nearest_yCF = YCF[min_distance_CFlabel]
-    nearest_probability = probabilities_CF[min_distance_CFlabel]
-    nearest_distance_obj = distances[min_distance_CFlabel]
-    nearest_dx_position = dx_positions[min_distance_CFlabel]
-    nearest_xpair = dxpairs[min_distance_CFlabel]
-    nearest_dxF = dxFs[min_distance_CFlabel]
-    nearest_dxCF = dxCFs[min_distance_CFlabel]
-    print(f'the cell type closest to cell type {query_data[-1]} is cell type {min_distance_CFlabel} \n'
-          f'the probability of xCF predicted as cell type {min_distance_CFlabel} is {round(nearest_probability*100, 2)}% \n'
-          f'nearest distance is {nearest_distance_obj} \n'
-          f'the position of change is at {nearest_dx_position} \n'
-          f'nearest dxpair: {nearest_dxpair} \n'
-          f'nearest dxF   : {nearest_dxF} \n'
-          f'nearest dCF   : {nearest_dxCF}'
-          )
-    return nearest_xCF, nearest_yCF, nearest_distance_obj, nearest_dx_position, nearest_dxF, nearest_dxCF, min_distance_CFlabel
-
+def solve_CF_with_diff_alpha(formulation, query_data, CFlabel, alphas):
+    xCF_dic = {}
+    yCF_dic = {}
+    probability_CF_dic = {}
+    probability_F_dic = {}
+    distance_obj_dic = {}
+    dx_position_dic = {}
+    dxpair_dic = {}
+    dxF_dic = {}
+    dxCF_dic = {}
+    last_probability_CF = 0
+    for alpha in alphas:
+        xCF, yCF, probability_CF, probability_F, distance_obj, dx, dx_position, dxpair, dxF, dxCF = solve_CF(formulation, query_data,
+                                                                                              CFlabel, alpha)
+        if probability_CF - last_probability_CF > 0.01:
+            xCF_dic[alpha] = xCF
+            yCF_dic[alpha] = yCF
+            probability_CF_dic[alpha] = probability_CF
+            probability_F_dic[alpha] = probability_F
+            distance_obj_dic[alpha] = distance_obj
+            dx_position_dic[alpha] = dx_position
+            dxpair_dic[alpha] = dxpair
+            dxF_dic[alpha] = dxF
+            dxCF_dic[alpha] = dxCF
+            last_probability_CF = probability_CF
+    return xCF_dic, yCF_dic, probability_CF_dic, probability_F_dic, distance_obj_dic, dx_position_dic, dxpair_dic, dxF_dic, dxCF_dic
 def get_cell_types():
     print("0: B cell \n"
           "1: epithelial cell \n"
@@ -214,18 +223,32 @@ def get_cell_types():
           "9: granulocyte \n"
           "10: rand \n")
 
-# use the first cell in test data as an example
+"""
+you can choose your factual and counterfactual here
+query_data = test_data[i] # the ith data point in test data
+make sure check the factual label by:
+print(query_data[-1])
+CFlabel = i # i from 0 to 10
+"""
+# use the first cell in test data as an example (its label is cell type 5)
 query_data = test_data[0]
-CFlabel = 1.2 # choose the counterfactual cell type of interest here
-# set confidence factor alpha (> 1)
-alpha = 2
-xCF, yCF, probability_CF, distance_obj, dx, dx_position, dxpair, dxF, dxCF = solve_CF_one_CFlabel(query_data, CFlabel, alpha)
+CFlabel = 1 # choose the counterfactual cell type of interest here
 
-
-# nearest_xCF, nearest_yCF, nearest_distance_obj, nearest_dx_position, nearest_dxF, nearest_dxCF, min_distance_CFlabel = solve_CF_all_CFlabels(query_data, alpha)
-
-
-
+# set confidence factor alphas
+alphas = [0.01, 0.02, 0.04, 0.08,
+          0.1, 0.2, 0.3,
+          0.6, 0.8,
+          1.2, 1.8,
+          3, 5, 8, 10,
+          15, 20, 30, 40, 50, 60,
+          100, 200, 300, 400
+          ]
+xCF_dic, yCF_dic, probability_CF_dic, probability_F_dic, distance_obj_dic, dx_position_dic, dxpair_dic, dxF_dic, dxCF_dic = solve_CF_with_diff_alpha(formulation, query_data, CFlabel, alphas)
+alpha_key = list(xCF_dic.keys())
+print(f'use the alpha key to access the attributes of interest:\n'
+      f'{alpha_key}')
+print(distance_obj_dic)
+print(probability_CF_dic)
 
 
 
